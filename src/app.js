e09b3eee3fafe90aaa2f1e46b5b459b063ac17d8
Haroon Arshad <arshadharoon216@gmail.com>
2025-04-21 22:54:57 +0500

    improved code

--- src/app.js
+++ src/app.js
@@ -14,7 +14,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors( {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(passport.initialize());
 app.use(cookieParser());
 
