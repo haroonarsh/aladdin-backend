1fa107606976bf1e86d833c770773f47fadcf973
Haroon Arshad <arshadharoon216@gmail.com>
2025-04-21 23:01:09 +0500

    fix: correct HTTP method for login route and add auth middleware for getUser route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,13 @@
-import { LoginUser, LogoutUser, RegisterUser } from "../controller/user.controller.js";
+import { GetUser, LoginUser, LogoutUser, RegisterUser } from "../controller/user.controller.js";
 import express from "express";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const route = express.Router();
 
 route.post("/signup", RegisterUser);
-route.get("/login", LoginUser);
+route.post("/login", LoginUser);
 route.post("/logout", LogoutUser);
+// route.get("/check", authMiddleware, CheckUser);
+route.get("/getUser", authMiddleware, GetUser); // Protect this route with the middleware
 
 export default route;
