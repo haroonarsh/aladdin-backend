0bdb8053f9506a94ddb28730d50ecd0da82089ee
Haroon Arshad <arshadharoon216@gmail.com>
2025-04-21 22:55:23 +0500

    adding more functionality like update user and get user.

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,14 +3,15 @@ import User from "../models/user.model.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import generateToken from "../utils/generateToken.js";
+import ApiError from "../utils/ApiError.js";
 
         // Register User
 const RegisterUser = asyncHandler(async (req, res) => {
     try {
-        const { FirstName, LastName, Email, PhoneNo, Password, Date, Gender } = req.body;
+        const { FirstName, LastName, Email, PhoneNo, Password, Gender } = req.body;
     
         if (!FirstName || !LastName || !Email || !PhoneNo || !Password) {
-            throw new Error("All fields are required");
+            throw new ApiError("All fields are required");
         }
 
         const existingUser = await User.findOne({ Email });
@@ -18,7 +19,7 @@ const RegisterUser = asyncHandler(async (req, res) => {
         const existingPassword = await User.findOne({ Password });
 
         if (existingUser || existingPhone || existingPassword) {
-            throw new Error("User already exists");
+            throw new ApiError("User already exists");
         }
     
         const user = await User.create({
@@ -27,21 +28,20 @@ const RegisterUser = asyncHandler(async (req, res) => {
             Email,
             PhoneNo,
             Password,
-            Date,
             Gender
         });
     
         if (!user) {
-            throw new Error("User registration failed");
+            throw new ApiError("User registration failed");
         }
 
         const { accessToken, refreshToken } = generateToken(user._id);
     
         if (!accessToken || !refreshToken) {
-            throw new Error("Token generation failed");
+            throw new ApiError("Token generation failed");
         }
     
-        res.cookie("jwt", refreshToken, {
+        res.cookie("jwt", accessToken, {
             httpOnly: true,
             secure: true,
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
@@ -53,7 +53,7 @@ const RegisterUser = asyncHandler(async (req, res) => {
         .json( new ApiResponse(201, { user }, "User registered successfully"));
     } catch (error) {
         console.log(error.message);
-        throw new Error(error.message || "User registration failed");
+        throw new ApiError(error.message || "User registration failed");
     }
 });
 
@@ -63,27 +63,27 @@ const LoginUser = asyncHandler(async (req, res) => {
         const { Email, Password } = req.body;
     
         if (!Email || !Password) {
-            throw new Error("All fields are required");
+            throw new ApiError("All fields are required");
         }
     
         const user = await User.findOne({ Email });
     
         if (!user) {    
-            throw new Error("User not found");
+            throw new ApiError("User not found");
         }
     
         const isPasswordMatch = await user.comparePassword(Password);
         if (!isPasswordMatch) {
-            throw new Error("Invalid password");
+            throw new ApiError("Invalid password");
         }
     
         const { accessToken, refreshToken } = generateToken(user._id);
     
         if (!accessToken || !refreshToken) {
-            throw new Error("Token generation failed");
+            throw new ApiError("Token generation failed");
         }   
     
-        res.cookie("jwt", refreshToken, {
+        res.cookie("jwt", accessToken, {
             httpOnly: true,
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
             secure: true,
@@ -95,7 +95,7 @@ const LoginUser = asyncHandler(async (req, res) => {
         .json( new ApiResponse(200, { user, accessToken, refreshToken }, "User logged in successfully"));
     } catch (error) {
         console.log(error.message);
-        throw new Error(error.message || "User login failed");
+        throw new ApiError(error.message || "User login failed");
     }
 });
 
@@ -114,15 +114,55 @@ const LogoutUser = asyncHandler(async (req, res) => {
 });
 
         // Update user
-// const UpdateUser = asyncHandler(async (req, res) => {
+const UpdateUser = asyncHandler(async (req, res) => {
+    try {
+        const { FirstName, LastName, Email, PhoneNo, Gender, Date, CountryCode} = req.body;
 
-//     const { id } = req.params;
+        if (!FirstName || !LastName || !Email || !PhoneNo) {
+            throw new Error("All fields are required");
+        }
 
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// })
+        const user = await User.findByIdAndUpdate(
+            req.user._id,
+            {
+                FirstName,
+                LastName,
+                Email,
+                PhoneNo,
+                Gender,
+                Date,
+                CountryCode
+            },
+            { new: true }
+        );
+        if (!user) {
+            throw new Error("User not found");
+        }
 
-export { RegisterUser, LoginUser, LogoutUser };
+        res
+        .status(200)
+        .json( new ApiResponse(200, { user }, "User updated successfully"));
+    } catch (error) {
+        console.log(error.message);
+        throw new Error(error.message || "User update failed");
+    }
+})
+
+
+    // Get User
+const GetUser = asyncHandler(async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select("-password -refreshToken");
+        if (!user) {
+            throw new Error("User not found");
+        }
+        res
+        .status(200)
+        .json( new ApiResponse(200, { user }, "User found successfully"));
+    } catch (error) {
+        console.log(error.message);
+        throw new Error(error.message || "User not found");
+    }
+})
+
+export { RegisterUser, LoginUser, LogoutUser, GetUser, UpdateUser };
