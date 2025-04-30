7af01cec67596c0adc0837dfe744658f6564d49e
Haroon Arshad <arshadharoon216@gmail.com>
2025-04-30 23:08:21 +0500

    refactor: reorganize middleware setup and import statements in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import passport from 'passport';
+// import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import session from 'express-session';
+import passport from './config/passport.js';
 
 dotenv.config(
     {
@@ -12,6 +14,7 @@ dotenv.config(
 
 const app = express();
 
+    // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors( {
@@ -19,17 +22,34 @@ app.use(cors( {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
-app.use(passport.initialize());
 app.use(cookieParser());
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+            sameSite: 'none',
+            secure: true,
+        }
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // Connect to DB
 import connectDB from './config/db.js';
 connectDB();
 
+// google routes
+import googleRouter from './routes/auth.routes.js';
+app.use('/auth', googleRouter);
 
 // Routes
 import userRouter from './routes/user.routes.js';
+// import passport from 'passport';
 app.use('/api/user', userRouter);
 
 // Start server
