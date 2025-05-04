4dda705d6fc63cef5a83eb1b90713124f1f2f8bb
Haroon Arshad <arshadharoon216@gmail.com>
2025-05-04 23:50:49 +0500

    feat: add multer middleware for profile image upload in updateImage route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { GetUser, LoginUser, LogoutUser, RegisterUser, UpdateImage, UpdateUser } from "../controller/user.controller.js";
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const route = express.Router();
 
@@ -10,6 +11,6 @@ route.post("/logout", LogoutUser);
 // route.get("/check", authMiddleware, CheckUser);
 route.get("/getUser", authMiddleware, GetUser); // Protect this route with the middleware
 route.put("/updateUser", authMiddleware, UpdateUser);
-route.put("/updateImage", authMiddleware, UpdateImage); // Protect this route with the middleware
+route.put("/updateImage", authMiddleware, upload.single("ProfileImage"), UpdateImage); // Protect this route with the middleware
 
 export default route;
