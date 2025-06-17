abe02840c17369c697bf377f87cb052960b5a55b
Haroon Arshad <arshadharoon216@gmail.com>
2025-06-18 00:22:36 +0500

    add cart routes for retrieving and deleting items from the cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { AddToCart } from "../controller/cart.controller.js";
+import { AddToCart, DeleteCart, GetCart } from "../controller/cart.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const route = express.Router();
 
 route.post("/add", authMiddleware, AddToCart);
+route.get("/get", authMiddleware, GetCart);
+route.delete("/remove/:id", authMiddleware, DeleteCart);
 
 export default route;
