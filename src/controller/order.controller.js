8c66cd5d35638efd242a6d1d7c5e04c19c0b182c
Haroon Arshad <arshadharoon216@gmail.com>
2025-07-24 00:16:20 +0500

    feat: implement order creation and status update functionality

---
+++ src/controller/order.controller.js
@@ -0,0 +1,96 @@
+import stripe from "../config/stripe.js";
+import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const createOrder = asyncHandler(async (req, res) => {
+    const { products, shippingAddress, name, email, phone, city, state } = req.body;
+    const userId = req.user._id;
+
+    if (!products || products.length === 0) {
+        return res.status(400).json({ message: "No products in the order" });
+    }
+
+    if (!shippingAddress) {
+        return res.status(400).json({ message: "Shipping address is required" });
+    }
+
+    try {
+        let totalAmount = 0;
+        for (const item of products) {
+            const product = await Product.findById(item.productId);
+            if (!product) {
+                return res.status(404).json({ message: `Product with ID ${item.productId} not found` });
+            }
+            if (product.stock < item.quantity) {
+                return res.status(400).json({ message: `Insufficient stock for product ${product.name}` });
+            }
+            totalAmount += product.price * item.quantity;
+        }
+
+        const amountInCents = Math.round(totalAmount * 100);
+
+        const order = new Order({
+            userId,
+            name,
+            email,
+            phone,
+            city,
+            state,
+            products,
+            totalAmount,
+            shippingAddress,
+            paymentStatus: "Pending",
+            orderStatus: "Pending"
+        });
+        await order.save();
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amountInCents, // Stripe expects the amount in cents
+            currency: 'usd',
+            metadata: { orderId: order._id.toString() }
+        });
+        
+        res
+        .status(201)
+        .json(new ApiResponse(201, { 
+            order, 
+            clientSecret: paymentIntent.client_secret },
+            "Order created successfully"
+        ));
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({
+            message: error.message || "Failed to create order",
+        });
+    }
+});
+
+const updateOrderStatus = asyncHandler(async (req, res) => {
+    const { orderId, paymentStatus, orderStatus } = req.body;
+
+    try {
+        const order = await Order.findById(orderId);
+        if (!order) {
+            return res.status(404).json({ message: "Order not found"});
+        }
+
+        if (paymentStatus) order.paymentStatus = paymentStatus;
+        if (orderStatus) order.orderStatus = orderStatus;
+
+        await order.save();
+
+        res
+        .status(200)
+        .json(new ApiResponse(200, { order }, "Order updated successfully"));
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({
+            message: error.message || "Failed to update order",
+        });
+    }
+})
+
+export { createOrder, updateOrderStatus };
