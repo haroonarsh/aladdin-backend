8d00fa180aabfde2a57b803d130a6154411435e6
Haroon Arshad <arshadharoon216@gmail.com>
2025-07-29 00:18:40 +0500

    feat: add fetchAllOrders and fetchOrderedProducts routes to order API

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createOrder, updateOrderStatus } from "../controller/order.controller.js";
+import { createOrder, fetchAllOrders, fetchOrderedProducts, updateOrderStatus } from "../controller/order.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const route = express.Router();
 
 route.post("/create", authMiddleware, createOrder);
 route.put("/update-status", authMiddleware, updateOrderStatus);
+route.get("/fetch-all", authMiddleware, fetchAllOrders);
+route.get("/fetch-ordered-products", authMiddleware, fetchOrderedProducts);
 
 export default route;
