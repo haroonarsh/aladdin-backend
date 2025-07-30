5fd693366bf52bfd6c9acb0271f572b64ee45f5c
Haroon Arshad <arshadharoon216@gmail.com>
2025-07-31 00:03:46 +0500

    feat: add GetProductsById route to product API for user-specific product retrieval

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { GetProducts, AddProduct, UpdateProduct, DeleteProduct, GetProduct } from "../controller/product.controller.js";
+import { GetProducts, AddProduct, UpdateProduct, DeleteProduct, GetProduct, GetProductsById } from "../controller/product.controller.js";
 import upload from "../middlewares/upload.middleware.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import admin from "../middlewares/admin.middleware.js";
@@ -7,6 +7,7 @@ import admin from "../middlewares/admin.middleware.js";
 const route = express.Router();
 
 route.get("/", GetProducts);
+route.get("/products", authMiddleware, admin, GetProductsById);
 route.post("/", authMiddleware, admin, upload.single("ProductImage"), AddProduct);
 
 route.get("/:id", GetProduct);
