617d73cb63efb43703c9070bb87100c041ecaea7
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-06 12:18:22 +0500

    feat: add fetchAdminOrders route to order API for admin order retrieval

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, fetchAllOrders, fetchOrderedProducts, updateOrderStatus } from "../controller/order.controller.js";
+import { createOrder, fetchAdminOrders, fetchAllOrders, fetchOrderedProducts, updateOrderStatus } from "../controller/order.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const route = express.Router();
@@ -8,5 +8,6 @@ route.post("/create", authMiddleware, createOrder);
 route.put("/update-status", authMiddleware, updateOrderStatus);
 route.get("/fetch-all", authMiddleware, fetchAllOrders);
 route.get("/fetch-ordered-products", authMiddleware, fetchOrderedProducts);
+route.get("/fetch-admin-orders", authMiddleware, fetchAdminOrders);
 
 export default route;
